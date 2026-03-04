43bcb9f88dd09e66e80508a233e76a5030057f7d
unknown <altikoddigitalsolutions>
2026-03-04 14:11:38 +0300

    Migration head sorunu düzeltildi

--- migrations/versions/35a3166540d2_recreate_production_tables_for_bom.py
+++ migrations/versions/35a3166540d2_recreate_production_tables_for_bom.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '35a3166540d2'
-down_revision = 'f1a2b3c4d5e7'
+down_revision = '240fad4e1bf2'
 branch_labels = None
 depends_on = None
 
